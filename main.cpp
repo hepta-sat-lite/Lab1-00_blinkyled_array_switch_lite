6c419462b76e8adb966398c89864416675d02ef5
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-06 05:08:24 +0000

    commit66

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+DigitalOut myled[]={LED1, PB_1};
+int main()
+{
+    int i=0;
+    while (true) {
+        switch(i){
+            case 1:
+                myled[0] = 1;
+                break;
+            case 2:
+                myled[1] = 1;
+                break;
+            case 3:
+                myled[0] = 0;
+                myled[1] = 0;
+                i = 0;
+                break;   
+        }
+        i++;
+        wait_ms(500);
+    }
 }
